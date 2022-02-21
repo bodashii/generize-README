b5291b8ffc69f965c6fe9aeee1c95cd99323c1bd
maddnty <nzkb24@gmail.com>
2022-02-20 19:58:56 -0600

    added prompts required to generate readme.md file returning  user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,95 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter a name for the project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project. (Required)',
+            validate: descriptionInput => {
+                if(descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of the project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please give installation instructions for the project. (Required)',
+            validate: instructionsInput => {
+                if (instructionsInput){
+                    return true;
+                } else {
+                    console.log('Please enter instructions for the project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please specify usage information for your project. (Required)',
+            validate: usageInput => {
+                if(usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of the project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: ' Please specify the contribution guidelines of your project? (Required)',
+            validate: contributionInput => {
+                if (contributionInput){
+                    return true;
+                } else {
+                    console.log('Please specify the contribution guidelines for the project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are your test instructions for the project. (Required)',
+            validate: descriptionInput => {
+                if(descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter test instructions for the project.');
+                    return false;
+                }
+            }
+        }
+    ]);
+    console.log(questions);
+};
+    // .then(generateData => {
+    //     questionData.question.push(generateData);
+    //     console.log(questions);
+    // }
 
+questions()
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
